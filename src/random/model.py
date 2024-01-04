36b33e0aa0f42a30ba213e9060aa575e322ed2a1
Konippi <konippi-114@outlook.jp>
2024-01-04 19:21:09 +0900

    add: utils

---
+++ src/random/model.py
@@ -0,0 +1,16 @@
+# import numpy as np
+# from util import path
+import os
+
+# class Random:
+#     def recommend(self, dataset, **kwargs):
+#         unique_user_ids = sorted(dataset.train.user_id.unique())
+
+#     def run(self):
+#         self.recommend(dataset)
+
+
+if __name__ == "__main__":
+    print(os.path.abspath(__file__))
+    # model = Random()
+    # model.run()
