e065b5fa437b3f605edb20125aeb02197c134724
Konippi <konippi-114@outlook.jp>
2024-01-06 15:32:42 +0900

    add: docstring

--- src/recommend/util/dataset_loader.py
+++ src/recommend/util/dataset_loader.py
@@ -10,6 +10,20 @@ class DatasetLoader:
         self.user_nums = user_nums
 
     def load(self) -> Tuple[pd.DataFrame, pd.DataFrame]:
+        """
+        Load the all datasets
+
+        Parameters
+        ----------
+            None
+
+        Returns
+        -------
+            movies: pd.DataFrame
+                movies(index, movie_id, title, genres, tags)
+            ratings: pd.DataFrame
+                ratings(index, user_id, movie_id, rating, timestamp)
+        """
         movies = self.__load_movies()
         ratings = self.__load_ratings()
         tags = self.__load_tags()
@@ -24,6 +38,26 @@ class DatasetLoader:
     def __preprocess(
         self, movies: pd.DataFrame, ratings: pd.DataFrame, tags: pd.DataFrame
     ) -> Tuple[pd.DataFrame, pd.DataFrame]:
+        """
+        Preprocess the datasets
+
+        Parameters
+        ----------
+            movies: pd.DataFrame
+                movies(index, movie_id, title, genres)
+            ratings: pd.DataFrame
+                ratings(index, user_id, movie_id, rating, timestamp)
+            tags: pd.DataFrame
+                tags(index, user_id, movie_id, tag, timestamp)
+
+        Returns
+        -------
+            movies: pd.DataFrame
+                movies(index, movie_id, title, genres, tags)
+            ratings: pd.DataFrame
+                ratings(index, user_id, movie_id, rating, timestamp)
+        """
+
         # unify tags to lowercase
         tags["tag"] = tags.tag.str.lower()
 
@@ -42,7 +76,7 @@ class DatasetLoader:
 
     def __load_movies(self) -> pd.DataFrame:
         """
-        Load the dataset
+        Load the movies dataset
 
         Parameters
         ----------
@@ -51,7 +85,7 @@ class DatasetLoader:
         Returns
         -------
             movies: pd.DataFrame
-                movies(movie_id, title, genres)
+                movies(index, movie_id, title, genres)
         """
         movie_cols = ["movie_id", "title", "genres"]
         movies = pd.read_csv(
@@ -65,7 +99,7 @@ class DatasetLoader:
 
     def __load_ratings(self) -> pd.DataFrame:
         """
-        Load the dataset
+        Load the ratings dataset
 
         Parameters
         ----------
@@ -74,7 +108,7 @@ class DatasetLoader:
         Returns
         -------
             ratings: pd.DataFrame
-                ratings(user_id, movie_id, rating, timestamp)
+                ratings(index, user_id, movie_id, rating, timestamp)
         """
         rating_cols = ["user_id", "movie_id", "rating", "timestamp"]
         ratings = pd.read_csv(
@@ -85,7 +119,7 @@ class DatasetLoader:
 
     def __load_tags(self) -> pd.DataFrame:
         """
-        Load the dataset
+        Load the tags dataset
 
         Parameters
         ----------
@@ -94,7 +128,7 @@ class DatasetLoader:
         Returns
         -------
             tags: pd.DataFrame
-                tags(user_id, movie_id, tag, timestamp)
+                tags(index, user_id, movie_id, tag, timestamp)
         """
         tag_cols = ["user_id", "movie_id", "tag", "timestamp"]
         tags = pd.read_csv(
